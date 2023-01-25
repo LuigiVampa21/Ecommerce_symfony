7eb002771ed4ddc7ec9de133f0fdd8e1e69ad6a6
Nathan Teissier <nmteissier@gmail.com>
2023-01-25 05:13:24 +0100

    register && send mail

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
 use App\Service\JWTService;
 use App\Service\SendMailService;
@@ -28,7 +29,8 @@ class RegistrationController extends AbstractController
                             UsersAuthenticator $authenticator, 
                             EntityManagerInterface $entityManager,
                             SendMailService $mail,
-                            JWTService $jwt
+                            JWTService $jwt,
+                            EntityManagerInterface $em
                             ): Response
     {
         $user = new Users();
@@ -60,7 +62,7 @@ class RegistrationController extends AbstractController
             $token = $jwt->generate($header,$payload,$this->getParameter('app.jwtsecret'));
 
             $mail->send(
-                'no-reply@adelfos.net',
+                'no-reply@adelfos.com',
                 $user->getEmail(),
                 'Email verification',
                 'register',
@@ -82,8 +84,72 @@ class RegistrationController extends AbstractController
         /**
          * @Route("/verif/{token}", name="verify_user")
          */
-    public function verifyUser($token, JWTService $jwt): Response
+    public function verifyUser($token, 
+                                JWTService $jwt, 
+                                UsersRepository $usersRepository, 
+                                EntityManagerInterface $em
+                                ): Response
     {
-        dd($jwt->getPayload($token));
+        if($jwt->isValid($token) &&
+          !$jwt->isExpired($token) && 
+           $jwt->check($token, $this->getParameter('app.jwtsecret')))
+           {
+                $payload = $jwt->getPayload($token);
+
+                $user = $usersRepository->find($payload['user_id']);
+
+                if($user && !$user->getIsVerified()){
+                    $user->setIsVerified(true);
+                    $em->flush($user);
+                    $this->addFlash('success', 'User activated');
+                    return $this->redirectToRoute('app_profile_index');
+                }
+            }
+        $this->addFlash('danger', 'Token is invalid or expired');
+        return $this->redirectToRoute('app_login');
     }
+
+
+            /**
+         * @Route("/resendverif", name="resend_verif")
+         */
+        public function resendVerif(
+                                    SendMailService $mail, 
+                                    JWTService $jwt, 
+                                    UsersRepository $usersRepository 
+                                    )
+        {
+            $user = $this->getUser();
+            if(!$user){
+                $this->addFlash('danger', 'You need to be connected to acceess this route.');
+                return $this->redirectToRoute('app_login');    
+            }
+
+            if($user->getIsVerified()){
+                $this->addFlash('warning', 'User already actived');
+                return $this->redirectToRoute('app_profile_index');    
+            }
+            
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+            
+            $payload = [
+            'user_id' => $user->getId()
+            ];
+
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            $mail->send(
+                'no-reply@adelfos.com',
+                $user->getEmail(),
+                'Email verification',
+                'register',
+                compact('user', 'token')
+            );
+            $this->addFlash('success', 'Verification email sent');
+            return $this->redirectToRoute('app_profile_index');
+    }
+
 }
--- src/Service/JWTService.php
+++ src/Service/JWTService.php
@@ -16,32 +16,31 @@ class JWTService
 
     public function generate(array $header, array $payload, string $secret, int $validity = 3600) :string
     {
-        if($validity <= 0){
-            return "";
+        if($validity > 0){
+            $now = new DateTimeImmutable();
+            $exp = $now->getTimestamp() + $validity;
+            
+            $payload['iat'] = $now->getTimestamp();
+            $payload['exp'] = $exp;
         }
-
-        $now = new DateTimeImmutable();
-        $exp = $now->getTimestamp() + $validity;
-
-        $payload['iat'] = $now->getTimestamp();
-        $payload['exp'] = $exp;
-
-        $base64Header = base64_encode(json_encode($header));
-        $base64Payload = base64_encode(json_encode($payload));
-
-        $base64Header = str_replace(['+', '/', '='], ['-', '_', ''], $base64Header);
-        $base64Payload = str_replace(['+', '/', '='], ['-', '_', ''], $base64Payload);  
-        
-        $secret = base64_encode($secret);
-        
-        $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
-        
-        $base64Signature = base64_encode($signature);
-        $base64Signature = str_replace(['+', '/', '='], ['-', '_', ''], $base64Signature);  
-
-        $jwt = $base64Header . '.' . $base64Payload . '.' . $base64Signature;
-        
-        return $jwt;
+            
+            
+            $base64Header = base64_encode(json_encode($header));
+            $base64Payload = base64_encode(json_encode($payload));
+            
+            $base64Header = str_replace(['+', '/', '='], ['-', '_', ''], $base64Header);
+            $base64Payload = str_replace(['+', '/', '='], ['-', '_', ''], $base64Payload);  
+            
+            $secret = base64_encode($secret);
+            
+            $signature = hash_hmac('sha256', $base64Header . '.' . $base64Payload, $secret, true);
+            
+            $base64Signature = base64_encode($signature);
+            $base64Signature = str_replace(['+', '/', '='], ['-', '_', ''], $base64Signature);  
+            
+            $jwt = $base64Header . '.' . $base64Payload . '.' . $base64Signature;
+            
+            return $jwt;
     }
 
     public function isValid(string $token) :bool 
@@ -52,6 +51,15 @@ class JWTService
         ) === 1;
     }
 
+    public function getHeader(string $token): array
+    {
+        $array = explode('.', $token);
+
+        $header = json_decode(base64_decode($array[0]), true);
+
+        return $header;
+    }
+
     public function getPayload(string $token) :array
     {
         $array = explode('.', $token);
@@ -59,5 +67,24 @@ class JWTService
         $payload = json_decode(base64_decode($array[1]), true);
 
         return $payload;
+    }
+
+    public function isExpired(string $token): bool
+    {
+        $payload = $this->getPayload($token);
+
+        $now = new DateTimeImmutable();
+
+        return $payload['exp'] < $now->getTimestamp();
+    }
+
+    public function check(string $token, string $secret)
+    {
+        $header = $this->getHeader($token);
+        $payload = $this->getPayload($token);
+
+        $verifToken = $this->generate($header, $payload, $secret, 0);
+
+        return $token === $verifToken;
     }
 }
