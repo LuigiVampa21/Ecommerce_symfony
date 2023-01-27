4fcf30cdb52fdab84fd87f0d0c003053f24c21f2
Nathan Teissier <nmteissier@gmail.com>
2023-01-27 08:46:56 +0100

    addProduct_form

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProductsFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -27,7 +28,16 @@ class ProductsController extends AbstractController
     public function add(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+
+        $product = new Products();
+
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        // return $this->render('admin/products/add.html.twig', [
+        //     'productForm' => $productForm->createView()
+        // ]);
+
+        return $this->renderForm('admin/products/add.html.twig', compact('productForm'));
     }
     
     /**
---
+++ src/Form/ProductsFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categories;
+use App\Entity\Products;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver; 
+
+class ProductsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('stock')
+            ->add('categories', EntityType::class, [
+                'class' => Categories::class,
+                'choice_label' => 'name'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
