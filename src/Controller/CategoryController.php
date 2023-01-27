69b305dc79f1f408c762d202d47a39a456ac0d0e
Nathan Teissier <nmteissier@gmail.com>
2023-01-27 06:17:24 +0100

    pagination_doctrine

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,9 +17,13 @@ class CategoryController extends AbstractController
     /**
      * @Route("/{slug}", name="list")
      */
-    public function details(Categories $category): Response
+    public function details(Categories $category, ProductsRepository $productsRepository, Request $request): Response
     {
-        $products = $category->getProducts();
+        $page = $request->query->getInt('page', 1);
+
+        // $products = $category->getProducts();
+        $products = $productsRepository->findProductsPaginated($page, $category->getSlug(), 2);
+
         return $this->render('categories/list.html.twig', compact('category', 'products'));
     }
 }
--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Products;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -21,23 +22,54 @@ class ProductsRepository extends ServiceEntityRepository
         parent::__construct($registry, Products::class);
     }
 
-    public function add(Products $entity, bool $flush = false): void
+    public function findProductsPaginated(int $page, string $slug, int $limit = 6): array
     {
-        $this->getEntityManager()->persist($entity);
+        $limit = abs($limit);
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
+        $result = [];
+
+        $query = $this->getEntityManager()->createQueryBuilder()
+                        ->select('c', 'p')
+                        ->from('App\Entity\Products', 'p')
+                        ->join('p.categories', 'c')
+                        ->where("c.slug = '$slug'")
+                        ->setMaxResults($limit)
+                        ->setFirstResult(($page *$limit) - $limit);
+
+        $paginator = new Paginator($query);
+        $data = $paginator->getQuery()->getResult();
+
+        if(empty($data)){
+            return $result;
         }
+
+        $pages = ceil($paginator->count() / $limit);
+
+        $result['data'] = $data;
+        $result['pages'] = $pages;
+        $result['page'] = $page;
+        $result['limit'] = $limit;
+
+        return $result;
     }
 
-    public function remove(Products $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
+    // public function add(Products $entity, bool $flush = false): void
+    // {
+    //     $this->getEntityManager()->persist($entity);
 
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
+    //     if ($flush) {
+    //         $this->getEntityManager()->flush();
+    //     }
+    // }
+
+    // public function remove(Products $entity, bool $flush = false): void
+    // {
+    //     $this->getEntityManager()->remove($entity);
+
+    //     if ($flush) {
+    //         $this->getEntityManager()->flush();
+    //     }
+    // }
 
 //    /**
 //     * @return Products[] Returns an array of Products objects
